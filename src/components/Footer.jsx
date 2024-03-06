68e09dad093e9421a4d62c4c9d36a76577822ddf
MateusProDev <mateusferreiraprodev@gmail.com>
2024-03-06 15:40:27 -0300

    add style ao footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,7 @@ import styles from "./Footer.module.css";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
+      <hr id="lineFooter" />
       <h3>Compartilhe suas ideias, conhecimentos e o que você tem interesse!</h3>
       <p>Blog APCE &copy; 2024 | Todos os direitos reservados.</p>
     </footer>
